12dd8dedb9ada857fa2a175c5a45b16c5d99ae2e
Maxime Cappellen <maxime.cappellen@intech.lu>
2025-02-19 22:45:57 +0100

    wip

--- setup.sh
+++ setup.sh
@@ -30,7 +30,9 @@ mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
 echo "Symlinking the configuration files ..."
 
-# Symlink the zshrc configuration file
+# Backup the existing zshrc file
+mv "$HOME"/.zshrc "$HOME"/.zshrc.bak
+# Symlink the zshrc configuration file and force it if it exists
 ln -s "$PWD"/.zshrc "$HOME"/.zshrc
 # Symlink the nvim configuration file
 ln -s "$PWD"/nvim "$XDG_CONFIG_HOME"/nvim
@@ -40,7 +42,8 @@ ln -s "$PWD"/config.nix "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 ln -s "$PWD"/tmux.conf "$HOME"/.tmux.conf
 
 # Reload zsh
-source "$HOME"/.zshrc
+#source "$HOME"/.zshrc
+exec zsh -l
 
 # Install Nix packages from config.nix
 nix-env -iA nixpkgs.myPackages
