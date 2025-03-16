0a95ca6a8ba148d8fee4fcc1dd3f3af9e7b3bb65
Maxime Cappellen <maxime.cappellen@intech.lu>
2025-03-16 23:11:04 +0100

    feat: a lot of stuff

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    enabled = vim.env.COPILOT == "copilot",
+  },
+  {
+    "blink-cmp-copilot",
+    enabled = vim.env.COPILOT == "copilot",
+  },
+  {},
+}
