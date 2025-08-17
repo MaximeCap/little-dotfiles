de6055d0f39edb2aebbeef1a2f14a24c34d06d8c
Max Cappellen <maxime.cappellen@icloud.com>
2025-08-17 20:25:24 +0200

    Add ts_ls, setup inlay hints for Golang

--- mini-nvim/lua/plugins/lsp.lua
+++ mini-nvim/lua/plugins/lsp.lua
@@ -5,13 +5,16 @@ function M.setup()
 		{ src = 'https://github.com/neovim/nvim-lspconfig' },
 		{ src = "https://github.com/mason-org/mason.nvim" },
 		{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-		{ src = 'https://github.com/stevearc/conform.nvim' }
+		{ src = 'https://github.com/stevearc/conform.nvim' },
+		{ src = 'https://github.com/j-hui/fidget.nvim' }
 	})
+
+	require "fidget".setup()
 
 	-- Install LSP
 	require "mason".setup()
 	require "mason-lspconfig".setup {
-		ensure_installed = { "lua_ls", "gopls" }
+		ensure_installed = { "lua_ls", "gopls", "ts_ls" }
 	}
 
 	-- Remove warning with global variable vim
@@ -43,15 +46,42 @@ function M.setup()
 			})
 		end,
 		settings = {
-			Lua = {}
+			Lua = {
+				hint = {
+					enable = true
+				}
+			}
 		}
 	})
+
+	vim.lsp.config("gopls", {
+		settings = {
+			gopls = {
+				["inlayHints.hints"] = {
+					parameterNames = true
+				}
+			}
+		}
+
+	})
+
+	-- vim.lsp.config("*", {
+	-- 	capabilities = {
+	-- 		textDocument = {
+	-- 			inlayHint = true
+	-- 		}
+	-- 	}
+	-- })
 
 	vim.api.nvim_create_autocmd("LspAttach", {
 		callback = function(ev)
 			local client = vim.lsp.get_client_by_id(ev.data.client_id)
 			if client ~= nil and client:supports_method('textDocument/completion') then
 				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+			end
+
+			if client ~= nil and client:supports_method('textDocument/inlayHint') then
+				vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
 			end
 		end
 	})
