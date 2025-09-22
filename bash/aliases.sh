b8f4432733bc9da7af48bc0de092ccc8385342ab
Max Cappellen <maxime.cappellen.e@thalesdigital.io>
2025-09-22 11:36:32 +0200

    fix and add function and aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -4,3 +4,18 @@ alias lt='eza --tree --level=2 --long --icons --git'
 alias lta='lt -a'
 alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
 alias cd="zd"
+
+zd() {
+  if [ $# -eq 0 ]; then
+    builtin cd ~ && return
+  elif [ -d "$1" ]; then
+    builtin cd "$1"
+  else
+    z "$@" && printf "\U000F17A9 " && pwd || echo "Error: Directory not found"
+  fi
+}
+
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
