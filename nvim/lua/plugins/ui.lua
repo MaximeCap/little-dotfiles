08bd1b293c569b423579ff8a22e7e38a1e244ef4
Max Cappellen <maxime.cappellen.e@thalesdigital.io>
2025-09-26 18:04:58 +0200

    change tmux + add keymap change between colors

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,75 +1,8 @@
 local M = {}
 
--- local theme_file_path = vim.fn.expand("$HOME/.theme")
--- local uv = vim.uv
---
---
--- local function read_file(path)
--- 	local file = io.open(path, "r")
--- 	if not file then
--- 		return "dark"
--- 	end
--- 	local content = file:read("*line")
--- 	file:close()
--- 	return content
--- end
---
---
--- local function set_light()
--- 	vim.o.background = "light"
--- end
---
--- local function set_dark()
--- 	vim.o.background = "dark"
--- end
---
--- local function watch_theme_change()
--- 	local handle = uv.new_fs_event()
---
--- 	local unwatch_cb = function()
--- 		if handle then
--- 			uv.fs_event_stop(handle)
--- 		end
--- 	end
---
--- 	local event_cb = function(err)
--- 		if err then
--- 			error("Theme file watcher failed")
--- 			unwatch_cb()
--- 		else
--- 			vim.schedule(function()
--- 				local theme = read_file(theme_file_path)
--- 				if theme == "light" then
--- 					set_light()
--- 				else
--- 					set_dark()
--- 				end
--- 				unwatch_cb()
--- 				watch_theme_change()
--- 			end)
--- 		end
--- 	end
---
--- 	local flags = {
--- 		watch_entry = false,
--- 		stat = false,
--- 		recursive = false
--- 	}
---
--- 	if handle then
--- 		uv.fs_event_start(handle, theme_file_path, flags, event_cb)
--- 	end
---
--- 	return handle
--- end
---
--- local theme = read_file(theme_file_path)
--- watch_theme_change()
-
 function M.setup()
 	require "gruvbox".setup {
 		terminal_colors = true,
-		dim_active = true
 	}
 
 	require "lualine".setup({
@@ -94,6 +27,8 @@ function M.setup()
 	whichKey.setup()
 
 	-- vim.keymap.set("n", "<leader>?", whichKey.show({ global = false }))
+	vim.keymap.set("n", "<leader>ul", ":lua vim.o.background = \"light\"")
+	vim.keymap.set("n", "<leader>ud", ":lua vim.o.background = \"dark\"")
 
 
 	--vim.o.background = theme
