64e731fc03922544bc1820846beafa12c85d888b
Max Cappellen <maxime.cappellen.e@thalesdigital.io>
2026-06-11 08:57:39 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export XDG_CONFIG_HOME="$HOME"/.config
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
 
 if ! command -v brew --version >/dev/null 2>&1
 then
